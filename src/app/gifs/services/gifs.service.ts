44b52bf7be60cc2bab60738cdc27f1a4a80f2388
Ignacio Merino <ignacio.merino@gmail.com>
2023-05-19 14:39:56 +0200

    carga imagenes

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,10 +8,12 @@ export class GifsService {
     public gifList: Gif[] = [];
 
     private _tagsHistory: string[] = [];
-    private apiKey: string = 'dGMJW12nlHSz46O3sy81BZFryoPshmUk'
+    private apiKey: string = 'ZYOf7OCWIWfvrHZSXvJ5bYp9fCYTOu83';
     private serviceURL: string = 'https://api.giphy.com/v1/gifs';
 
-    constructor( private http: HttpClient) { }
+    constructor( private http: HttpClient) { 
+        this.loadLocalStorage();
+    }
     
     get tagsHistory() {
         // Como se pasa por referencia, lo podrian modificar
@@ -30,6 +32,21 @@ export class GifsService {
 
         this._tagsHistory.unshift(tag);
         this._tagsHistory = this._tagsHistory.splice(0,10);
+
+        this.saveLocalStorage();
+    }
+
+    private saveLocalStorage():void {
+        localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+    }
+
+    private loadLocalStorage():void {
+        const temporal = localStorage.getItem('history');
+        if(temporal) {
+            this._tagsHistory = JSON.parse(temporal);
+
+            this.searchTag(this._tagsHistory[0]);
+        }
     }
 
     public searchTag( tag:string ):void  {
@@ -47,9 +64,9 @@ export class GifsService {
 
         this.http.get<SearchResponse>(`${ this.serviceURL }/search`, { params })
             .subscribe(resp => { 
-                console.log(resp);
+                //console.log(resp);
                 this.gifList = resp.data;
-                console.log( { gifs: this.gifList } ) ;
+                //console.log( { gifs: this.gifList } ) ;
             });
 
 
